52e903eafc006d921228e6df9bce6d3529793c42
Young-ook Kim <youngook.kim@gmail.com>
2022-04-21 00:38:18 +0900

    feat(eks): awsfis eks example

---
+++ examples/eks/main.tf
@@ -0,0 +1,72 @@
+terraform {
+  required_version = "~> 1.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# network/vpc
+module "vpc" {
+  source  = "Young-ook/vpc/aws"
+  version = "1.0.1"
+  name    = var.name
+  tags    = var.tags
+  vpc_config = var.use_default_vpc ? null : {
+    cidr        = var.cidr
+    azs         = var.azs
+    subnet_type = "isolated"
+    single_ngw  = true
+  }
+}
+
+# application/eks
+module "eks" {
+  source             = "Young-ook/eks/aws"
+  name               = var.name
+  tags               = var.tags
+  subnets            = values(module.vpc.subnets["private"])
+  kubernetes_version = var.kubernetes_version
+  enable_ssm         = true
+  fargate_profiles = [
+    {
+      name      = "loadtest"
+      namespace = "loadtest"
+    },
+  ]
+  managed_node_groups = [
+    {
+      name          = "sockshop"
+      desired_size  = 3
+      min_size      = 3
+      max_size      = 9
+      instance_type = "t3.small"
+    }
+  ]
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.helmconfig.host
+    token                  = module.eks.helmconfig.token
+    cluster_ca_certificate = base64decode(module.eks.helmconfig.ca)
+  }
+}
+
+module "container-insights" {
+  source       = "Young-ook/eks/aws//modules/container-insights"
+  cluster_name = module.eks.cluster.name
+  oidc         = module.eks.oidc
+}
+
+module "cluster-autoscaler" {
+  source       = "Young-ook/eks/aws//modules/cluster-autoscaler"
+  cluster_name = module.eks.cluster.name
+  oidc         = module.eks.oidc
+}
+
+module "metrics-server" {
+  source       = "Young-ook/eks/aws//modules/metrics-server"
+  cluster_name = module.eks.cluster.name
+  oidc         = module.eks.oidc
+}
