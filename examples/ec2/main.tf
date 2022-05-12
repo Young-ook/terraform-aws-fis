de4987f5b1ff1535b91517d13b83f1265f52833a
Young-ook Kim <youngook.kim@gmail.com>
2022-05-13 08:30:58 +0900

    fix(ec2): remove loadgen ec2 from alb target group

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -139,9 +139,9 @@ module "ec2" {
   node_groups = [
     {
       name              = "baseline"
-      min_size          = 2
+      min_size          = 3
       max_size          = 6
-      desired_size      = 2
+      desired_size      = 3
       instance_type     = "t3.small"
       security_groups   = [aws_security_group.alb_aware.id]
       target_group_arns = [aws_lb_target_group.http.arn]
@@ -168,7 +168,6 @@ module "ec2" {
       desired_size      = 1
       instance_type     = "t3.small"
       security_groups   = [aws_security_group.alb_aware.id]
-      target_group_arns = [aws_lb_target_group.http.arn]
       policy_arns       = ["arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"]
       user_data         = local.client
     }
