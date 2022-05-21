2abd8b93b12d4109ebb21dc1fa93fd75d6ffa65c
youngookkim <youngook.kim@gmail.com>
2022-05-21 11:04:36 +0900

    feat(rds): mysql connection error fallback (#21)

--- examples/rds/lampapp/www/index.php
+++ examples/rds/lampapp/www/index.php
@@ -2,7 +2,7 @@
  <head>
   <title>Hello...</title>
 
-  <meta charset="utf-8"> 
+  <meta charset="utf-8">
 
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
@@ -15,9 +15,8 @@
 
     <?php
 
-    // mysql connection
+    // Create MySQL connection
     $conn = mysqli_connect($_ENV["MYSQL_HOST"], $_ENV["MYSQL_USER"], $_ENV["MYSQL_PASSWORD"], $_ENV["MYSQL_DB"]);
-
 
     $query = 'SELECT * From Person';
     $result = mysqli_query($conn, $query);
@@ -25,18 +24,18 @@
     echo '<table class="table table-striped">';
     echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
     while($value = $result->fetch_array(MYSQLI_ASSOC)){
-        echo '<tr>';
-        echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
-        foreach($value as $element){
-            echo '<td>' . $element . '</td>';
-        }
-
-        echo '</tr>';
+    echo '<tr>';
+      echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+      foreach($value as $element){
+        echo '<td>' . $element . '</td>';
+      }
+      echo '</tr>';
     }
     echo '</table>';
 
     $result->close();
 
+    // Close connection
     mysqli_close($conn);
 
     ?>
