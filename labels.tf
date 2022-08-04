fd6af9a127118339407516b2c1fecf4c7e3e43e8
youngookkim <youngook.kim@gmail.com>
2022-08-04 14:39:18 +0900

    feat(fis): upgrade frigga naming module version (#37)

--- labels.tf
+++ labels.tf
@@ -1,7 +1,7 @@
 ### frigga name
 module "frigga" {
   source  = "Young-ook/spinnaker/aws//modules/frigga"
-  version = "2.3.3"
+  version = "2.3.4"
   name    = var.name == null || var.name == "" ? "fis" : var.name
   petname = var.name == null || var.name == "" ? true : false
 }
